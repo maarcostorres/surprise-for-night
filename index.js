c6938e7ade82c25abf99f0930c8453d7769d4063
Marcos Vinicius Torres <97858422+maarcostorres@users.noreply.github.com>
2023-07-23 20:58:20 -0300

    a

---
+++ index.js
@@ -0,0 +1,4 @@
+document.getElementById("btnBoaNoite").addEventListener("click", function() {
+    var gifContainer = document.getElementById("gifContainer");
+    gifContainer.classList.add("show");
+  });
